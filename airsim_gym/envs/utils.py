849da66902429930369bc327fff9d7c35fdbb5d1
alireza nobakht <a.nobakht13@gmail.com>
2022-04-19 09:26:28 +0430

    get_orthogonal_vectors was checked successfully

--- airsim_gym/envs/utils.py
+++ airsim_gym/envs/utils.py
@@ -1,4 +1,5 @@
 from airsim.types import Pose,Quaternionr,Vector3r
+import airsim as air
 import numpy as np
 
 def get_orthogonal_vectors(qr: Quaternionr):
@@ -29,4 +30,24 @@ def get_orthogonal_vectors(qr: Quaternionr):
 
 
 if __name__ == "__main__":
+    drone = air.MultirotorClient()
+    drone.reset()
+    drone.enableApiControl(True)
+    drone.armDisarm(True)
+
+    drone.takeoffAsync().join()
+
+    obj_pose = drone.simGetObjectPose("myobject")
+
+    obj_crd = obj_pose.position
+    obj_orn = obj_pose.orientation
+
+    print(f"position: x={obj_crd.x_val}, y={obj_crd.y_val}, z={obj_crd.z_val}")
+    print(f"orientation: w={obj_orn.w_val}, x={obj_orn.x_val}, y={obj_orn.y_val}, z={obj_orn.z_val}")
     
+    e1,e2,e3 = get_orthogonal_vectors(obj_orn)
+    print(f"e1 = {e1}\ne2 = {e2}\ne3 = {e3}")
+
+    drone.armDisarm(False)
+    drone.reset()
+    drone.enableApiControl(False)
